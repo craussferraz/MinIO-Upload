2625c69f82b8324fc1d1c15da25e7e0ce969d999
Crauss Ferraz <crauss.ferraz@gmail.com>
2020-07-01 16:19:39 -0300

    atualizando a install.sh #31

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ wget https://dl.min.io/client/mc/release/linux-amd64/mc -O /usr/local/bin/mc
 chmod +x /usr/local/bin/minio
 chmod +x /usr/local/bin/mc
 # inicializando o minio
-if [ `ps -eF | grep minio | grep -v grep |wc -l`==1 ];then
+if [ `ps -eF | grep minio | grep -v grep |wc -l` == 1 ];then
 	kill -9 `pidof minio`
 fi
 minio --quiet server /srv/data &
@@ -21,7 +21,7 @@ sleep 13
 # configurando o minio
 mc config host add myminio http://$SERVERIP:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 # criando bucket docs
-if [ `mc ls myminio |grep docs |wc -l`!=1 ];then
+if [ `mc ls myminio |grep docs |wc -l` != 1 ];then
         mc mb myminio/docs
 fi
 # baixando e instalando o minio_uploader
@@ -29,7 +29,7 @@ cd /srv
 if [ -e minio_uploader.zip ]; then
 	rm -rf minio_uploade*
 fi
-if [ `ps -eF | grep wsgi:app | grep -v grep |wc -l`!=0 ]; then
+if [ `ps -eF | grep wsgi:app | grep -v grep |wc -l` != 0 ]; then
    kill -9 `ps -eF |grep wsgi:app | grep -v grep |  awk '{print $2}'| awk '{a=$0;printf "%s ",a,$0}'`
 fi
 wget https://github.com/craussferraz/MinIO-Upload/raw/master/minio_uploader.zip
