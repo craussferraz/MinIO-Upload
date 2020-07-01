1d9905d9e7cef4e2230bd0c8db7dc17d1964912e
Crauss Ferraz <crauss.ferraz@gmail.com>
2020-07-01 14:17:53 -0300

    atualizando a master #24

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@
 # baixando dependendicas
 apt update
 apt install 
-apt install -y python3 python3-pip wget unzip 
+apt install -y python3 python3-pip wget unzip
 
 source export.sh
-
 
 # Baixando o minio.
 wget https://dl.minio.io/server/minio/release/linux-amd64/minio -O /usr/local/bin/minio
@@ -14,9 +13,9 @@ wget https://dl.min.io/client/mc/release/linux-amd64/mc -O /usr/local/bin/mc
 chmod +x /usr/local/bin/minio
 chmod +x /usr/local/bin/mc
 # inicializando o minio
-if [ `ps -eF | grep minio | grep -v grep |wc -l` == 1 ];then
-	sudo kill -9 `pidof minio`
-if
+if [ `ps -eF | grep minio | grep -v grep |wc -l`==1 ];then
+	kill -9 `pidof minio`
+fi
 minio --quiet server /srv/data &
 sleep 13
 # configurando o minio
@@ -28,9 +27,13 @@ cd /srv
 if [ -e minio_uploader.zip ]; then
 	rm -rf minio_uploade*
 fi
+if [ `ps -eF | grep wsgi:app | grep -v grep |wc -l`!=0 ]; then
+   kill -9 `ps -eF |grep wsgi:app | grep -v grep |  awk '{print $2}'| awk '{a=$0;printf "%s ",a,$0}'`
+fi
 wget https://github.com/craussferraz/MinIO-Upload/raw/master/minio_uploader.zip
 unzip minio_uploader.zip
 cd minio_uploader
 pip3 install pipenv
 pipenv --python 3 install --system --deploy
-gunicorn --bind 0.0.0.0:5000 wsgi:app &
+gunicorn -D --bind 0.0.0.0:5000 wsgi:app &
+echo done
